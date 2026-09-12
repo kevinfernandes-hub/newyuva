ec2e168651bb980b634a0386e8296fd29edac15e
Kevin Fernandes <fernandeskevin860@gmail.com>
2026-09-12 16:01:14 +0530

    Add semantic retrieval package

---
+++ backend/retrieval/__init__.py
@@ -0,0 +1,5 @@
+"""Semantic satellite imagery retrieval components for SIH26227."""
+
+from .encoder import RemoteSensingEncoder, RemoteCLIPEncoder
+
+__all__ = ["RemoteSensingEncoder", "RemoteCLIPEncoder"]
